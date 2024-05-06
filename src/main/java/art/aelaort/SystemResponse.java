8b66db75d42631051723371e123abda6fe8e9d02
ort <orthodoxplus@ya.ru>
2024-05-06 18:15:58 +0300

    ffprobe

---
+++ src/main/java/art/aelaort/SystemResponse.java
@@ -0,0 +1,4 @@
+package art.aelaort;
+
+public record SystemResponse(String stdout, String stderr) {
+}
--- src/main/java/art/aelaort/ffprobe/FFprobe.java
+++ src/main/java/art/aelaort/ffprobe/FFprobe.java
@@ -1,13 +1,14 @@
 package art.aelaort.ffprobe;
 
 import art.aelaort.FFmpegPaths;
-import art.aelaort.SystemCalls;
-import art.aelaort.ffprobe.models.FFprobeResult;
+import art.aelaort.SystemResponse;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class FFprobe {
-	public static FFprobeResult probe(String mediaPath) {
+	public static SystemResponse probe(String mediaPath) {
 		List<String> command = List.of(
 				FFmpegPaths.FFPROBE_PATH,
 				"-v", "quiet",
@@ -18,12 +19,34 @@ public class FFprobe {
 				"-show_chapters",
 				mediaPath
 		);
-		Process process = SystemCalls.callPrintableDebug(command);
+		return call(command);
+	}
+
+	private static SystemResponse call(List<String> command) {
 		try {
-			process.waitFor();
-		} catch (InterruptedException e) {
+			Process p = new ProcessBuilder(command).start();
+
+			StringBuilder stdout = new StringBuilder();
+			try (BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+				String line;
+				while ((line = b.readLine()) != null) {
+					stdout.append(line);
+				}
+			}
+
+			StringBuilder stderr = new StringBuilder();
+			try (BufferedReader b = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+				String line;
+				while ((line = b.readLine()) != null) {
+					stderr.append(line);
+				}
+			}
+
+			p.waitFor();
+
+			return new SystemResponse(stdout.toString(), stderr.toString());
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		return null;
 	}
 }
