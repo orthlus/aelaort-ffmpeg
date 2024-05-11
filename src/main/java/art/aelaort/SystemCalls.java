357568d9ec7092a42fe5695f0b90ef7004016d2b
ort <orthodoxplus@ya.ru>
2024-05-11 16:51:36 +0300

    clean

--- src/main/java/art/aelaort/SystemCalls.java
+++ src/main/java/art/aelaort/SystemCalls.java
@@ -1,11 +1,8 @@
 package art.aelaort;
 
-import org.jooq.lambda.tuple.Tuple2;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class SystemCalls {
 	public static Process callPrintableDebug(List<String> command) {
@@ -55,35 +52,6 @@ public class SystemCalls {
 			}
 
 			return p;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public static Tuple2<String, String> callTuple(List<String> command) {
-		try {
-			Process p = new ProcessBuilder(command)
-					.inheritIO()
-					.start();
-			p.waitFor(2, TimeUnit.MINUTES);
-
-			StringBuilder stdout = new StringBuilder();
-			try (BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-				String line;
-				while ((line = b.readLine()) != null) {
-					stdout.append(line);
-				}
-			}
-
-			StringBuilder stderr = new StringBuilder();
-			try (BufferedReader b = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-				String line;
-				while ((line = b.readLine()) != null) {
-					stderr.append(line);
-				}
-			}
-
-			return new Tuple2<>(stdout.toString(), stderr.toString());
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
